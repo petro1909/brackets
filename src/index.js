29b86c85e0286f4d171423d2f967a48135c8b734
petro1909 <petya-97_97@mail.ru>
2019-03-20 20:11:07 +0300

    feat: almost solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+ 
+  var ch;
+  var stack = [];
+
+  var openingBrackets = ['[', '{', '(','1','3','5','7','8'];
+  var closingBrackets = [']', '}', ')','2','4','6','7','8'];
+  var samebrackets=['|'];
+    if (str.length <= 1)
+      return false;
+
+    for (var i = 0; i < str.length; i++) {
+      ch = str[i];
+  
+      if (closingBrackets.indexOf(ch) != -1) {
+        bracketsConfig = openingBrackets[closingBrackets.indexOf(ch)];
+        if (stack.length == 0 || (stack.pop() != bracketsConfig)) {
+          return false;
+        }
+      } else {
+        stack.push(ch)
+      }
+    }
+
+    return (stack.length == 0)
+  };
+  
+ 
+
+
+ 
+
